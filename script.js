0c18205731ca871ab5f9066ffdf30a45c6807f90
Sujal R Kale <kalesujal6325@gmail.com>
2026-07-21 12:16:58 +0530

    update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ let btn = document.querySelector("button");
 let body = document.querySelector("body");
 
 btn.addEventListener("click", function(evt){
-  if (body.classList.contain === body.light){
-    body.classList.remove("body.light");
-    body.classList.add("body.dark");
+  if (body.classList.contain("light")){
+    body.classList.remove("light");
+    body.classList.add("dark");
   } else {
-    body.classList.remove("body.dark");
-    body.classList.add("body.light");
+    body.classList.remove("dark");
+    body.classList.add("light");
   }
 })
