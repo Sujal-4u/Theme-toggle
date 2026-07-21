ef219f81b94c55ff4821c3229b97c554efa87b65
Sujal R Kale <kalesujal6325@gmail.com>
2026-07-21 12:31:02 +0530

    update script.js

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 let btn = document.querySelector("button");
 let body = document.querySelector("body");
-
+let theme = window.matchMedia('(prefers-color-scheme: dark)');
+function setDarkOrLight(){
+  if (theme.matches){
+    body.classList.remove("dark");
+    body.classList.add("light");
+  } else {
+    body.classList.remove("light");
+    body.classList.add("dark");
+  }
+}
+if (localStorage.getItem("theme")){
+  body.classList.add(localStorage.getItem("theme"))
+} else {
+  setDarkOrLight();
+}
 btn.addEventListener("click", function(evt){
   if (body.classList.contains("light")){
     body.classList.remove("light");
     body.classList.add("dark");
+    localStorage.setItem("theme", "dark");
   } else {
     body.classList.remove("dark");
     body.classList.add("light");
+    localStorage.setItem("theme", "light");
   }
 })
